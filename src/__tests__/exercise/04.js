3ff6eb844c6781a95796208ffd9fed88964eaecd
Erickowski <daniel_perez_03081995@hotmail.com>
2022-10-23 17:58:44 -0500

    04 extra 2 exercise solved

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,14 +5,18 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import faker from 'faker'
 
 test('submitting the form calls onSubmit with username and password', async () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
   // const handleSubmit = data => (submittedData = data)
-  const username = 'fakeUsername'
-  const password = 'fakePassword'
+  const buildLoginForm = () => ({
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  })
+  const {username, password} = buildLoginForm()
   // let submittedData = {}
   const handleSubmit = jest.fn()
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
