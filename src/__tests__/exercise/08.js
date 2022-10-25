94466aa2dba93890bc2c891649c0d204d3c16244
Erickowski <daniel_perez_03081995@hotmail.com>
2022-10-24 19:16:38 -0500

    08 exercise solved

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -11,12 +11,32 @@ import useCounter from '../../components/use-counter'
 // capabilities of this hook
 // 💰 here's how to use the hook:
 // const {count, increment, decrement} = useCounter()
+const TestComponent = () => {
+  const {count, increment, decrement} = useCounter()
 
-test('exposes the count and increment/decrement functions', () => {
+  return (
+    <>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <p>The count is: {count}</p>
+    </>
+  )
+}
+
+test('exposes the count and increment/decrement functions', async () => {
   // 🐨 render the component
+  render(<TestComponent />)
   // 🐨 get the elements you need using screen
+  const increment = screen.getByText(/increment/i)
+  const decrement = screen.getByText(/decrement/i)
+  const message = screen.getByText(/the count is:/i)
   // 🐨 assert on the initial state of the hook
+  expect(message).toHaveTextContent('The count is: 0')
   // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  await userEvent.click(increment)
+  expect(message).toHaveTextContent('The count is: 1')
+  await userEvent.click(decrement)
+  expect(message).toHaveTextContent('The count is: 0')
 })
 
 /* eslint no-unused-vars:0 */
